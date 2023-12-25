f3d787a9a77fe18bc059d8c87e288f8e46bcc3df
NatashaMoses <117117440+NatashaMoses@users.noreply.github.com>
2023-12-25 11:32:01 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Natasha Upendo Moses",
+  title: "Data Science & AI",
+  email: "natmos910@gmail.com",
+  gitHub: "NatashaMoses",
+  instagram: "Natasha Upendo Moses",
+  linkedIn: "Natasha Upendo Moses",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,26 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "src/images/pink bg.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "pink marble background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Bsc. Mathematics and Computer Science student at JKUAT. I have specialised in the Data Science track and I enjoy leveraging my AI skills to help come up with solutions to real world scenarios.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Data Science",
+  "Machine Learning",
+  "Artificial Intelligence",
+  "Web development",
+  "Regression & Classification",
 ];
 
 /**
