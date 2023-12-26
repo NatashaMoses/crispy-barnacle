b1e3ed3ec670cf3776547516cfac020c7b4c9a80
NatashaMoses <117117440+NatashaMoses@users.noreply.github.com>
2023-12-26 09:00:48 +0000

    commit final

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "/workspaces/crispy-barnacle/src/images/pink bg.jpg";
+import image from "../images/pink bg.jpg";
 
 const imageAltText = "pink marble background";
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "/workspaces/crispy-barnacle/src/images/happywoman.webp";
+import image from "../images/happywoman.webp";
 
 const imageAltText = "Woman using her laptop outdoors while smiling";
 
